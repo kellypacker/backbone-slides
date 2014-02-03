e5739cc9021026f3b5625c2864437abf64f3532f
Kelly Packer <kelly@creativesoapbox.com>
2014-02-03 13:52:08 -0700

    added setFontSize method for slides

--- app/scripts/models/slide.js
+++ app/scripts/models/slide.js
@@ -5,6 +5,24 @@ define(['backbone'], function(Backbone) {
     defaults: {
       type: 'note',
       title: ''
+    },
+
+    initialize: function() {
+      this.setFontSize();
+    },
+
+    setFontSize: function() {
+      var length = this.get("title").length;
+      console.log(length)
+      var size;
+      if ( length >= 80 ) {
+        size = 'x-large';
+      } else if ( length >= 40 ) {
+        size = 'large';
+      } else {
+        size = 'normal';
+      }
+      this.set('size', size);
     }
 
   });
