a7a188c5a75048a15329a52dd909d83d9a44591e
Kelly Packer <kelly@creativesoapbox.com>
2014-02-03 12:25:03 -0700

    added transition on slides

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -7,11 +7,15 @@ define([
 function(Backbone, SlidesView, SlidesCollection, MainRouter) {
   var AppView = Backbone.View.extend({
     el: 'body',
+    events: {
+      'keyup': 'keyUp'
+    },
 
     initialize: function () {
       var testCollection = [
         { title: 'first slide' },
-        { title: 'second slide' }
+        { title: 'second slide' },
+        { title: 'third slide' }
       ];
 
       App.router = new MainRouter();
@@ -21,7 +25,18 @@ function(Backbone, SlidesView, SlidesCollection, MainRouter) {
       });
 
       Backbone.history.start();
+    },
+
+    keyUp: function(e) {
+      // 37 = left
+      // 39 = right
+      if ( e.keyCode === 37 || e.keyCode === 39) {
+        App.Vent.trigger('changeSlide', {
+          direction: e.keyCode === 39 ? 'next' : 'prev'
+        });
+      }
     }
+
   });
 
   return AppView;
--- app/scripts/views/slides.js
+++ app/scripts/views/slides.js
@@ -4,12 +4,63 @@ define(['backbone', 'views/slide'], function(Backbone, SlideView){
 
     initialize: function() {
       this.renderAll();
-
+      this.currentSlideIndex = 1;
+      this.numSlides = this.collection.length;
+      this.transitionSpeed = 400;
       App.Vent.on('init', this.hideAllButFirst, this);
+      App.Vent.on('changeSlide', this.changeSlide, this)
     },
 
     hideAllButFirst: function() {
       this.$el.children(':nth-child(n+2)').hide();
+    },
+
+    changeSlide: function(opts) {
+      var self = this;
+      var newSlide;
+      var $slides = this.$el.children()
+      // If we are requesting a specific slide
+      // then set current index
+      if ( opts.slideIndex ){
+        this.currentSlideIndex = ~~opts.slideIndex;
+      } else {
+        // otherwise grab next or prev slide
+        this.setCurrentSlideIndex(opts.direction);
+      }
+
+      newSlide = $slides.eq(this.currentSlideIndex - 1);
+      // transition to new slide
+      $slides.filter(':visible')
+        .css('position', 'absolute')
+        .animate({
+          top: opts.direction === 'next' ? '100%' : '-100%',
+          opacity: 'hide'
+        }, this.transitionSpeed, function() {
+          //slide is gone
+          $(this).css('top', 0);
+          newSlide
+            .css('top', opts.direction === 'next' ? '-100%' : '100%')
+            .animate({
+              top: 0,
+              opacity: 'show'
+            }, self.transitionSpeed);
+        });
+
+      App.router. navigate('/slides/' + this.currentSlideIndex);
+
+    },
+
+    setCurrentSlideIndex: function(direction) {
+      this.currentSlideIndex += direction === 'next' ? 1 : -1;
+
+      if (this.currentSlideIndex > this.numSlides ) {
+        this.currentSlideIndex = 1;
+      }
+
+      if (this.currentSlideIndex <= 0) {
+        this.currentSlideIndex = this.numSlides;
+      }
+
     },
 
     renderAll: function() {
